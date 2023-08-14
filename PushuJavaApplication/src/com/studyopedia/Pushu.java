c9bb2f493db4b3f0d2c454fdb0218d20eff2840e
ashok <ashok@DESKTOP-DSRIM73>
2023-08-14 14:13:23 +0530

    first commit

---
+++ PushuJavaApplication/src/com/studyopedia/Pushu.java
@@ -0,0 +1,11 @@
+package com.studyopedia;
+
+public class Pushu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Pushu's first Java program...");
+
+	}
+
+}
